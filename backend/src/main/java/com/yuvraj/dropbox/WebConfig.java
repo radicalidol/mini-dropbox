c6888cbb950ddb019332d8d24a029ee50c8caff5
Apple <apple@Apples-MacBook-Pro.local>
2025-10-12 20:41:45 +0530

    allow server frontend test

--- backend/src/main/java/com/yuvraj/dropbox/WebConfig.java
+++ backend/src/main/java/com/yuvraj/dropbox/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","http://34.14.146.138:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
